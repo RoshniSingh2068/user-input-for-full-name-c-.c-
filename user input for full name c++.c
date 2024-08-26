1b991973100c7d1835e2994329299209e94c20c4
RoshniSingh2068 <roshnisingh1228@gmail.com>
2024-08-26 11:13:57 +0530

    Create user input for full name c++.c

---
+++ user input for full name c++.c
@@ -0,0 +1,10 @@
+# include <iostream>
+# include <string>
+using namespace std;
+int main(){
+    string fullName;
+    cout<< "Type your fullName " ;
+    getline(cin, fullName); //FOR THE FULLNAME OUTPUT
+    cout<<"your name is " << fullName << endl;
+    return 0;
+}
